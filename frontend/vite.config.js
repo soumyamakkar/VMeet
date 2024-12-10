922f33e3107e7f1e58aaabed93f84c97a5526415
SoumyaMakkar <soumyamakkar123@gmail.com>
2024-12-11 00:08:46 +0530

    arghhh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0", // Ensure the app listens on all network interfaces
+    port: process.env.PORT || 3000, // Use the provided port or fallback to 3000
+  },
   plugins: [react()],
 })
